987b959277b38fa7772661d899442833d98524bc
Christina Z <zhu.zhiyi@northeastern.edu>
2025-04-21 20:57:03 -0400

    add doors

--- Assets/Fries and Seagull/Interior 01/Scripts/Utility/YureiManagerURP.cs
+++ Assets/Fries and Seagull/Interior 01/Scripts/Utility/YureiManagerURP.cs
@@ -1,5 +1,49 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+#if UNITY_EDITOR
+using Seagull.Interior_01.Utility.Inspector;
+#endif
 
 namespace Seagull.Interior_01.Utility {
-    public class YureiManagerURP : MonoBehaviour { }
+    public class YureiManagerURP : MonoBehaviour {
+        public VolumeProfile volumeProfile;
+        
+        [Tooltip("Post Process effect including glowing will only show to these cameras")]
+        public List<Camera> gameCameras = new();
+        
+        [YureiButton("Initialize")] [IgnoreInInspector]
+        public Action initialize;
+
+        private void Reset() {
+            initialize = init;
+        }
+
+        private void init() {
+            if (gameCameras == null || gameCameras.Count == 0) {
+                Debug.LogError("Please provide at least 1 valid camera to Game Cameras field.");
+                return;
+            }
+            
+            foreach (var camera in gameCameras) {
+                camera.GetComponent<UniversalAdditionalCameraData>().renderPostProcessing = true;
+                Volume volume = camera.GetComponent<Volume>();
+                if (volume) volume.sharedProfile = volumeProfile;
+                else {
+                    volume = camera.gameObject.AddComponent<Volume>();
+                    volume.sharedProfile = volumeProfile;
+                }
+            }
+            Debug.Log($"[Yurei] Init post-processor settings for Universal Rendering Pipeline successfully.");
+        }
+
+    }
+    
+    #if UNITY_EDITOR
+    [CustomEditor(typeof(YureiManagerURP))]
+    public class YureiManagerURPInspector : YureiInspector { }
+    #endif
 }
